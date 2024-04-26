194a5ecf361ffcad909042a43be216dd5eab0a52
my name <blueshaperd3@gmail.com>
2024-04-26 20:01:38 +0500

    globel and local scope

--- app.js
+++ app.js
@@ -11,7 +11,19 @@
 // add(10,8,5); // 10,8,5 are called arguments
 
 // Return arguments sai value return krta hai
+// function add(a,b,c){
+//     var z = a+b+c
+//     return z;
+// }
+// var g = add(10,8,5);
+// alert(g)
+
+// Function with globel & local scope
+var f = 10; // this is globel scope
 function add(a,b,c){
+    alert(f)
+    var d = 99; // this is local scope
+    alert(d)
     var z = a+b+c
     return z;
 }
