31a8bd12ff43dd4c49ad96a3187956aa73f2ddfb
my name <blueshaperd3@gmail.com>
2024-04-26 19:32:18 +0500

    function with parameters and aurguments

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-function greeting(){
-    alert("Hello Rehan")
+// Main Function Structure
+// function greeting(){
+//     alert("Hello Rehan")
+// }
+// greeting();
+
+// Function with parameters & arrguments
+function add(a,b,c){
+    alert(a+b+c)
 }
-greeting();
+add(10,8,5); // 10,8,5 are called arguments
