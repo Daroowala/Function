0d62c00ac0f52172a6fe5b7f4c239ab8ad0ef3c5
my name <blueshaperd3@gmail.com>
2024-04-26 21:54:47 +0500

    local and globel variables in details

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 // }
 // add(10,8,5); // 10,8,5 are called arguments
 
+
 // Return arguments sai value return krta hai
 // function add(a,b,c){
 //     var z = a+b+c
@@ -18,14 +19,37 @@
 // var g = add(10,8,5);
 // alert(g)
 
+
 // Function with globel & local scope
-var f = 10; // this is globel scope
-function add(a,b,c){
-    alert(f)
-    var d = 99; // this is local scope
-    alert(d)
-    var z = a+b+c
-    return z;
+// var f = 10; // this is globel scope
+// function add(a,b,c){
+//     alert(f)
+//     var d = 99; // this is local scope
+//     alert(d)
+//     var z = a+b+c
+//     return z;
+// }
+// var g = add(10,8,5);
+// alert(g)
+
+
+
+// agar variable globel hai to uski value change kr skte hai aur globel variable ko function ka andher aur bahir dono tarikoon sai check kar sakta haan
+// var a = "Rehan";
+// function foo(){
+//      a = "Ali"
+// }
+// // foo();
+// console.log(a);
+
+
+
+// agar variable local hai to uski value change nhi kr skte hai aur local variable ko function call kar ka check kar sakta hai
+var a = "Rehan";
+function foo(){
+     a = "Ali"
+
 }
-var g = add(10,8,5);
-alert(g)
+
+foo();
+console.log(a);
